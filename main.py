61826996fa7386a9b1625aa229656e08180f7684
s26777 <s26777@pjwstk.edu.pl>
2024-03-15 13:15:08 +0100

    EX 3

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-def generate_squares(start, end):
-    """
-    Generate a list of squares for a given range of numbers.
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        """
+        Generate a list of squares for a given range of numbers.
 
-    Parameters:
-        start (int): The starting number of the range (inclusive).
-        end (int): The ending number of the range (inclusive).
+        Parameters:
+            start (int): The starting number of the range (inclusive).
+            end (int): The ending number of the range (inclusive).
 
-    Returns:
-        list: A list of squares for the given range of numbers.
-    """
-    squares = [x**2 for x in range(start, end + 1)]
-    return squares
+        Returns:
+            list: A list of squares for the given range of numbers.
+        """
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
 
 def main():
+    # Create an instance of the SquareGenerator class
+    square_gen = SquareGenerator()
+
     start = int(input("Enter the start of the range: "))
     end = int(input("Enter the end of the range: "))
 
-    squares = generate_squares(start, end)
+    # Call the generate_squares method of the SquareGenerator instance
+    squares = square_gen.generate_squares(start, end)
     print(f"List of squares from {start} to {end}:")
     print(squares)
 
