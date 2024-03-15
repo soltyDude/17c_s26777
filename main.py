bc07a333bf34974b99a27948fe4671d8d36b2a10
s26777 <s26777@pjwstk.edu.pl>
2024-03-15 13:19:01 +0100

    EX 6

--- main.py
+++ main.py
@@ -1,42 +1,4 @@
-import math
-
-
-class RangeError(Exception):
-    """Custom exception for invalid range."""
-    pass
-
-
-class SquareGenerator:
-    def generate_squares(self, start, end):
-        """
-        Generate a list of squares for a given range of numbers.
-
-        Parameters:
-            start (int): The starting number of the range (inclusive).
-            end (int): The ending number of the range (inclusive).
-
-        Returns:
-            list: A list of squares for the given range of numbers.
-        """
-        if end < start:
-            raise RangeError("End of the range must be greater than or equal to the start.")
-
-        squares = [x ** 2 for x in range(start, end + 1)]
-        return squares
-
-    def calculate_square_roots(self, numbers):
-        """
-        Calculate the square roots of each number in the list.
-
-        Parameters:
-            numbers (list): List of numbers.
-
-        Returns:
-            list: List of square roots of each number.
-        """
-        square_roots = [math.sqrt(num) for num in numbers]
-        return square_roots
-
+from square_generator import SquareGenerator, RangeError
 
 def main():
     square_gen = SquareGenerator()
@@ -55,7 +17,6 @@ def main():
 
     except RangeError as e:
         print("Error:", e)
-
 
 if __name__ == "__main__":
     main()
---
+++ square_generator.py
@@ -0,0 +1,38 @@
+import math
+
+
+class RangeError(Exception):
+    """Custom exception for invalid range."""
+    pass
+
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        """
+        Generate a list of squares for a given range of numbers.
+
+        Parameters:
+            start (int): The starting number of the range (inclusive).
+            end (int): The ending number of the range (inclusive).
+
+        Returns:
+            list: A list of squares for the given range of numbers.
+        """
+        if end < start:
+            raise RangeError("End of the range must be greater than or equal to the start.")
+
+        squares = [x ** 2 for x in range(start, end + 1)]
+        return squares
+
+    def calculate_square_roots(self, numbers):
+        """
+        Calculate the square roots of each number in the list.
+
+        Parameters:
+            numbers (list): List of numbers.
+
+        Returns:
+            list: List of square roots of each number.
+        """
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots
