7dc80e11675c4272c882e5e91ad58c52e13d3026
s26777 <s26777@pjwstk.edu.pl>
2024-03-15 13:14:56 +0100

    EX 2

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-def generate_squares():
-    # Generate a list of squares using list comprehension
-    squares = [x**2 for x in range(1, 11)]
+def generate_squares(start, end):
+    """
+    Generate a list of squares for a given range of numbers.
+
+    Parameters:
+        start (int): The starting number of the range (inclusive).
+        end (int): The ending number of the range (inclusive).
+
+    Returns:
+        list: A list of squares for the given range of numbers.
+    """
+    squares = [x**2 for x in range(start, end + 1)]
     return squares
 
 def main():
-    squares = generate_squares()
-    # Print the list of squares
-    print("List of squares from 1 to 10:")
+    start = int(input("Enter the start of the range: "))
+    end = int(input("Enter the end of the range: "))
+
+    squares = generate_squares(start, end)
+    print(f"List of squares from {start} to {end}:")
     print(squares)
 
 if __name__ == "__main__":
